d814749159bf336d6e4232edfc24cc773b25f483
tombh <tom@tombh.co.uk>
2011-05-24 00:47:47 +0100

    made some sick changes using Twitter's @anywhere stuff :)

--- index.php
+++ index.php
@@ -11,10 +11,12 @@
 
   <link rel="icon" href="/images/favicon.png" type="image/png" />
 
+  <script src="http://platform.twitter.com/anywhere.js?id=z49BVG9I8tfDr1IQNL9aaQ&amp;v=1"></script>
+
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js" type="text/javascript"></script>
 
   <script type="text/javascript" src="http://use.typekit.com/yri8oua.js"></script>
-  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>  
 
   <script type="text/javascript">
     var handle = '<?php echo $handle ?>';
@@ -28,23 +30,27 @@
 <body>
   <div id="container">
     <div id="logo">
-      <a href="/">
-        <h1>#openpractice</h1>
-      </a>
+      <h1>
+        <a href="/">
+          #openpractice
+        </a>
+      </h1>
     </div>
 
     <div id="info">
 
       <center><em><small>The paint is wet here, <a href="http://search.twitter.com/search?q=%23opsite">#opsite</a> wants to know what you think before it dries.</small></em></center>
+
+      
       
       <?php if(!empty($handle)) : ?>
         <center>
-          <h2>Just <a href="http://twitter.com/<?php echo $handle ?>" target="_blank">@<?php echo $handle ?></a>'s #openpractice tweets</h2>
+          <h2>Just <a href="http://twitter.com/<?php echo $handle ?>" target="_blank" class="tweep">@<?php echo $handle ?></a>'s #openpractice tweets</h2>
           <br />
           <strong><a href="/">Back to everyone &raquo;</a></strong>
         </center>
      <?php else : ?>
-
+        <div id="tweetbox"></div>
      <?php endif ?>
       
     </div>
@@ -53,12 +59,15 @@
       &nbsp;
       <ul>
         <li class="template" style="display:none">
-          <a class="avatar_link" href=""><img class="profile_pic" src="" width="52" height="52" /></a>
+          <a class="avatar_link tweep" href="" target="_blank"><img class="profile_pic" src="" width="52" height="52" title=""/></a>
           <div class="tweet_body">
           </div>
           <div class="meta">
-            By <strong><a href="" class="handle">@</a></strong> <abbr class="timeago" title=""></span>
+            By <strong><a href="" class="handle op_archive" title="">@</a></strong> <abbr class="timeago" title=""></abbr> |
+            <a href="#" class="retweet">ReTweet</a> |
+            <a href="#" class="reply">Reply</a>
           </div>
+          <div class="tweet" style="display: none"></div>
         </li>
       </ul>
     </div>
@@ -77,12 +86,16 @@
           <small>
           <ul>
             <li>Avatars link to user's Twitter accounts.</li>
-            <li>@{handles} link to user's #openpractice archive.</li>
+            <li><span class="op_archive">@{handles}</span> link to user's #openpractice archive.</li>
           </ul>
           </small>
         <?php endif; ?>
         
       </div>
+
+      <p>
+        <iframe src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fpages%2Fopenpractice%2F200626913315542&amp;width=165&amp;colorscheme=light&amp;show_faces=true&amp;stream=false&amp;header=false&amp;height=262" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:165px; height:262px;" allowTransparency="true"></iframe>
+      </p>
       </small>
     </div>
 
--- js/main.js
+++ js/main.js
@@ -1,11 +1,39 @@
+var refresh = true;
+
 if(handle.length > 0){
   var url = '/JSON_tweets.php?filter=' + handle;  
 }else{
   var url ='/tweets.json';
 }
 
-$(document).ready( function(){ 
+twttr.anywhere(function (T) {
+
+  T("#tweetbox").tweetBox({
+    height: 60,
+    width: 700,
+    label: "What's happening in your practice?",
+    defaultContent: "#openpractice "
+  });  
+
+});
+
+$(document).ready( function(){   
   getTweets();
+  $('.retweet').live('click', function(){
+    refresh = false;
+    $('.tweet').fadeOut().html('');
+    var tweep = $(this).parent().children('strong').children('a').html();
+    var body = trim(stripHTML($(this).parent().prev('.tweet_body').html()));
+    tweet($(this).parent().next('.tweet'), 'ReTweet ' + tweep, 'RT ' + tweep + ': ' + body);
+    return false;
+  });
+  $('.reply').live('click', function(){
+    refresh = false;
+    $('.tweet').fadeOut().html('');
+    var tweep = $(this).parent().children('strong').children('a').html();    
+    tweet($(this).parent().next('.tweet'), 'Reply to ' + tweep, tweep);
+    return false;
+  });
 });
 
 function getTweets(){
@@ -22,13 +50,36 @@ function getTweets(){
       tweets = eval(json);
       render_tweets(tweets);
       setTimeout(getTweets, 60000);
+      hovercards();
+      
+      $('.tweet_body a').click(function(){
+        var fullUrl = $(this).attr('href');
+        var splitUrl = fullUrl.split("/");       
+        if((splitUrl[2] != 'www.openpractice.me') && (splitUrl[2] != 'openpractice.me')){
+            $(this).attr("target","_blank");
+        }       
+      });
+      
     }    
   });
 }
 
+function hovercards(){
+  twttr.anywhere(function (T) {   
+    T('a.tweep').hovercards({      
+      expanded: true,
+      username: function(e) {        
+        return e.title;
+      }
+    });
+  });
+}
+
 function render_tweets(tweets){
+
+  if(!refresh) return;
   
-  $('#tweets ul li:not(.template)').fadeOut();
+  $('#tweets ul li:not(.template)').remove();
   
   for(var i = 0; i < tweets.length; i++){
 
@@ -38,12 +89,14 @@ function render_tweets(tweets){
 
     $('#tweets ul li:last').attr('class', '');
 
-    $('#tweets ul li:last .avatar_link').attr('href', '/' + tweets[i].handle);
+    $('#tweets ul li:last .avatar_link').attr('href', 'http://twitter.com/' + tweets[i].handle);
+    $('#tweets ul li:last .avatar_link img').attr('title', tweets[i].handle);
     $('#tweets ul li:last .profile_pic').attr('src', tweets[i].avatar);
     
     $('#tweets ul li:last .tweet_body').append(content);
     $('#tweets ul li:last .handle').append(tweets[i].handle);
     $('#tweets ul li:last .handle').attr('href', '/' + tweets[i].handle);
+    $('#tweets ul li:last .handle').attr('title', '@' + tweets[i].handle + '\'s #openpractice archive');
     $('#tweets ul li:last .timeago').attr('title', tweets[i].published);
     $('#tweets ul li:last .timeago').html(tweets[i].published);
     
@@ -62,4 +115,26 @@ function stripslashes(str) {
   str=str.replace(/\\0/g,'\0');
   str=str.replace(/\\\\/g,'\\');
   return str;
+}
+
+function stripHTML(html){
+   var tmp = document.createElement("DIV");
+   tmp.innerHTML = html;
+   return tmp.textContent||tmp.innerText;
+}
+
+function tweet(element, label, content){  
+  twttr.anywhere(function (T) {
+    T(element).tweetBox({
+      height: 60,
+      width: 500,
+      label: label,
+      defaultContent: content
+    });
+  });
+  $(element).fadeIn();
+}
+
+function trim(stringToTrim) {
+  return stringToTrim.replace(/^\s+|\s+$/g,"");
 }
