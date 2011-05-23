d755e1f4e5c868bb38a1537f0e74be25e3e6020d
tombh <tom@tombh.co.uk>
2011-05-24 00:57:27 +0100

    google analytics

--- index.php
+++ index.php
@@ -24,7 +24,21 @@
   </script>
 
   <script src="/js/main.js" type="text/javascript"></script>
-  <script src="/js/jquery.timeago.js" type="text/javascript"></script>  
+  <script src="/js/jquery.timeago.js" type="text/javascript"></script>
+
+  <script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-23465987-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+  </script>
 
 </head>
 
@@ -101,6 +115,7 @@
     </div>
 
   </div>
+  
 </body>
 
 </html>
