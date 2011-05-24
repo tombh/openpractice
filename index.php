31590e4d5602497f06776e1104d5f15aab9967e0
tombh <tom@tombh.co.uk>
2011-05-24 21:28:39 +0100

    added link to github

--- index.php
+++ index.php
@@ -112,6 +112,10 @@
       <p>
         <iframe src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fpages%2Fopenpractice%2F200626913315542&amp;width=165&amp;colorscheme=light&amp;show_faces=true&amp;stream=false&amp;header=false&amp;height=380" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:165px; height:380px;" allowTransparency="true"></iframe>
       </p>
+
+      <p>
+        <small>You can clone, fork and contribute to the code on this site at <a href="https://github.com/tombh/openpractice" target="_blank">Github</a>.</small>
+      </p>
       </small>
     </div>
 
